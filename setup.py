e0d25e60165a6b6edcfb216073e35422a50e50da
@eduardo_78d <eduardo78d@gmail.com>
2018-04-02 09:53:47 -0500

    changes

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
+
 setup(
   name = 'snake_progress',
   packages = ['snake_progress'],
-  version = '0.1',
+  version = '0.0.1',
   description = 'Class that allows to show a progress bar in console',
-  author = 'Eduardo Ismael García Pérez',
+  author = 'Eduardo Ismael Garcia Perez',
   author_email = 'eduardo78d@gmail.com',
   url = 'https://github.com/eduardogpg/snake_progress', 
-  keywords = ['progress', 'progress_bar', 'progress bar', 'console', 'output', 'download'],
-  classifiers = [],
+  keywords = ['progress', 'progress_bar', 'progress bar', 'console', 'output', 'download']
 )
