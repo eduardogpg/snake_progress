75f064cb55b3c2b943e7c6799119d3e2a75cfe8e
@eduardo_78d <eduardo78d@gmail.com>
2018-04-01 21:13:23 -0500

    first commit

---
+++ example.py
@@ -0,0 +1,32 @@
+from progress_bar import ProgressBar
+from time import sleep
+
+#First example
+#output
+#progress |██████████████████████████████████████████████████| 100.0% (100/100)
+
+print("First Example")
+progress_bar = ProgressBar(size=50, show_balance=True)
+
+items = list(range(0, 100))
+for i, item in enumerate(items):
+     sleep(0.1)
+     progress_bar.update_progress(i, len(items))
+
+progress_bar.complete()
+
+#Second example
+#output
+#Progress completed |🦖🦖🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕🦕|
+print("\n")
+print("Second Example")
+progress_bar = ProgressBar(size=20, fill='🦖', empty='🦕', prefix='Progress completed', show_percentage=False, show_balance=False)
+progress_bar.update_progress(10, 100, True)
+
+#Third example
+#output
+#progress |███████████         | 55.5% (55.5/100)
+print("\n")
+print("Third Example")
+progress_bar = ProgressBar(size=20)
+progress_bar.update_progress(55.5, 100, True)
