80338b0682670017201df87d18cbcf0614e5fdc2
@eduardo_78d <eduardo78d@gmail.com>
2018-04-02 21:46:34 -0500

    show progress bar in the same line

--- example.py
+++ example.py
@@ -1,9 +1,50 @@
 from snake_progress_bar import ProgressBar
 from time import sleep
 
+progress_bar = ProgressBar()
+items = list(range(0, 100))
+
+for i, item in enumerate(items):
+     sleep(0.1)
+     progress_bar.update(i * 100, len(items) * 100)
+
+progress_bar.complete()
+
+
+"""
 #First example
 #output
-print("First Example")
+#Progress |██████████████████                                                      | 25.0% (25/100)
+print("\n>First Example")
 
-progress_bar = ProgressBar(prefix='Progress completed')
-progress_bar.update(50, 100, True)
+progress_bar = ProgressBar() #Default Values
+progress_bar.update(25, 100, True) #Current 25, total 100, skip line True
+
+
+
+#Second example
+#output
+#Progress completed |///////                                                                   |   
+print("\n>Second Example")
+
+progress_bar = ProgressBar(fill='/', prefix='Progress completed', show_percentage=False, show_balance=False)
+progress_bar.update(10, 100, True)
+
+#Third example
+#output
+#Progress |******------------------------------------------------------| 10.0% (10/100
+print("\n>Third Example")
+
+progress_bar = ProgressBar(fill='*', empty='-', auto_bar_size=False, custome_bar_size=60)
+progress_bar.update(10, 100, True)
+
+
+#Fourth example
+#output
+#Progress |███████████████████████████████████████████████████████████████████████████████| 100.0%
+print("\n>Fourth Example")
+
+progress_bar = ProgressBar()
+progress_bar.complete()
+
+"""
--- snake_progress_bar/progress.py
+++ snake_progress_bar/progress.py
@@ -15,15 +15,14 @@ class ProgressBar(object):
         self.set_variables()
         self.terminal_size = self.terminal_size()
         
-
-    def set_variables(self, current=0, buffer=1, skip=False, completed=False):
+    def set_variables(self, current=0, buffer=1, skip=False):
         self.current = current
         self.buffer = buffer
         self.skip = skip
-        self.completed = completed
+        self.completed = self.current == self.buffer
 
     def percentage_completed(self):
-        percentage = (self.current * 100) / self.buffer
+        percentage = int(self.current * 100) / self.buffer
         return float(format(percentage, self.precision))
 
     def bar_size(self):
@@ -33,7 +32,7 @@ class ProgressBar(object):
             return self.custome_bar_size
 
     def bar_size_suggestion(self):
-        percentage_text = int( len(self.full_text()) * 100 ) / self.terminal_size
+        percentage_text = int( len(self.full_text() ) * 100 ) / self.terminal_size
         percentage_bar = int(100 - percentage_text)
 
         return int((percentage_bar * self.terminal_size) / 100)
@@ -54,7 +53,16 @@ class ProgressBar(object):
         return "\n" if self.completed or self.skip else ""
 
     def right_text(self):
-        return "{} {} {}".format(self.percentage_format(), self.balance_format(), self.skip_format())
+        return "{} {}".format(self.percentage_format(), self.balance_format())
+    
+    def full_right_text(self):
+        return "{}{}".format(self.right_text(), self.skip_format())
+
+    def full_text(self):
+        return "{}{}{}".format(self.prefix, self.right_text(), " " * 4)
+
+    def full_progress_bar(self):
+        return "\r{} |{}| {}".format(self.prefix, self.progress_bar(), self.full_right_text())
 
     def progress_bar(self):
         bar_size = self.bar_size()
@@ -65,12 +73,6 @@ class ProgressBar(object):
 
         return "{}{}".format(progress, empty)
 
-    def full_text(self):
-        return "{} {}".format(self.prefix, self.right_text())
-
-    def full_progress_bar(self):
-        return "{} |{}| {}".format(self.prefix, self.progress_bar(), self.right_text())
-
     def show_full_progress_bar(self):
         import sys
 
@@ -79,9 +81,14 @@ class ProgressBar(object):
 
     def update(self, current, buffer, skip=False):
         self.set_variables(current, buffer, skip)
-
         self.show_full_progress_bar()
 
+    def is_valid_show_balance(self):
+        return self.buffer != 1 and self.current != 0 if self.show_balance else False
+        
     def complete(self):
-        pass
+        self.set_variables(self.buffer, self.buffer)
+        self.show_balance = self.is_valid_show_balance()
+        
+        self.show_full_progress_bar()
 
